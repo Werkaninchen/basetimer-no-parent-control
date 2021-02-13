1f5b500bd9eedbfb1873df39c3849a35adc69db4
Jeroen Trzaska <jeroen@werkaninchen.de>
2021-02-13 22:29:08 +0100

    added controlls

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,123 @@
 <template>
-  <div id="app">
-    <BaseTimer/>
-  </div>
+    <div id="app">
+        <BaseTimer class="pb" v-bind:reps="reps" v-bind:timeLimit="lim" />
+        <div>
+            <div>
+                <div class="vert">
+                    <p>Work time:</p>
+                    <input
+                        type="number"
+                        name="work"
+                        id="work"
+                        v-model.number="work"
+                    />
+                </div>
+                <div class="vert">
+                    <p>Break time:</p>
+                    <input
+                        type="number"
+                        name="brk"
+                        id="brk"
+                        v-model.number="brk"
+                    />
+                </div>
+                <div class="vert">
+                    <p>repetitions</p>
+                    <input
+                        type="number"
+                        name="reps"
+                        id="reps"
+                        v-model.number="reps"
+                    />
+                </div>
+            </div>
+            <div>
+                <button v-on:click="start">Start</button>
+                <button v-on:click="reset">Reset</button>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
 import BaseTimer from "./components/BaseTimer";
+import { bus } from "./main";
 
 export default {
-  name: "App",
+    name: "App",
 
-  components: {
-    BaseTimer
-  }
+    data() {
+        return {
+            work: 20,
+            brk: 10,
+            reps: 8,
+            lim: 10,
+            is_break: false,
+            did_start: false,
+        };
+    },
+    components: {
+        BaseTimer,
+    },
+    mounted() {
+        bus.$on("inter_done", this.next);
+    },
+    methods: {
+        start() {
+            if (!this.did_start) {
+                this.lim = this.brk;
+                this.did_start = true;
+            }
+            this.is_break = !this.is_break;
+            console.log(this.lim);
+            console.log(this.is_break);
+            bus.$emit("start");
+        },
+        reset() {
+            this.did_start = false;
+            bus.$emit("reset");
+        },
+        next() {
+            if (this.reps <= 0) {
+                bus.$emit("completed");
+                this.did_start = false;
+                return;
+            }
+            if (this.is_break) {
+                this.lim = this.work;
+            } else {
+                this.lim = this.brk;
+                this.reps = this.reps - 1;
+            }
+
+            this.is_break = !this.is_break;
+            bus.$emit("start");
+        },
+    },
 };
 </script>
 
-<style>
-body {
-  background-color: #fff;
+<style lang="scss" >
+.vert {
+    display: flex;
 }
-
+body {
+    background-color: #00ff00;
+}
+.pb {
+    margin-bottom: 100vh;
+}
 #app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  width: 100%;
+    font-family: "Avenir", Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
 }
 </style>
--- src/components/BaseTimer.vue
+++ src/components/BaseTimer.vue
@@ -1,168 +1,176 @@
 <template>
-  <div class="base-timer">
-    <svg class="base-timer__svg" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
-      <g class="base-timer__circle">
-        <circle class="base-timer__path-elapsed" cx="50" cy="50" r="45"></circle>
-        <path
-          :stroke-dasharray="circleDasharray"
-          class="base-timer__path-remaining"
-          :class="remainingPathColor"
-          d="
+    <div class="base-timer">
+        <svg
+            class="base-timer__svg"
+            viewBox="0 0 100 100"
+            xmlns="http://www.w3.org/2000/svg"
+        >
+            <g class="base-timer__circle">
+                <circle
+                    class="base-timer__path-elapsed"
+                    cx="50"
+                    cy="50"
+                    r="45"
+                ></circle>
+                <path
+                    :stroke-dasharray="circleDasharray"
+                    class="base-timer__path-remaining"
+                    d="
             M 50, 50
             m -45, 0
             a 45,45 0 1,0 90,0
             a 45,45 0 1,0 -90,0
           "
-        ></path>
-      </g>
-    </svg>
-    <span class="base-timer__label">{{ formattedTimeLeft }}</span>
-  </div>
+                ></path>
+            </g>
+        </svg>
+        <span class="base-timer__label">{{ formattedTimeLeft }}</span>
+        <p class="base-timer__sub_label">{{ reps }}</p>
+    </div>
 </template>
 
 <script>
 const FULL_DASH_ARRAY = 283;
-const WARNING_THRESHOLD = 10;
-const ALERT_THRESHOLD = 5;
-
-const COLOR_CODES = {
-  info: {
-    color: "green"
-  },
-  warning: {
-    color: "orange",
-    threshold: WARNING_THRESHOLD
-  },
-  alert: {
-    color: "red",
-    threshold: ALERT_THRESHOLD
-  }
-};
-
-const TIME_LIMIT = 20;
+import { bus } from "../main";
 
 export default {
-  data() {
-    return {
-      timePassed: 0,
-      timerInterval: null
-    };
-  },
-
-  computed: {
-    circleDasharray() {
-      return `${(this.timeFraction * FULL_DASH_ARRAY).toFixed(0)} 283`;
+    props: {
+        timeLimit: Number,
+        reps: Number,
+    },
+    data() {
+        return {
+            timePassed: 0,
+            timerInterval: null,
+        };
     },
 
-    formattedTimeLeft() {
-      const timeLeft = this.timeLeft;
-      const minutes = Math.floor(timeLeft / 60);
-      let seconds = timeLeft % 60;
+    computed: {
+        circleDasharray() {
+            return `${(this.timeFraction * FULL_DASH_ARRAY).toFixed(0)} 283`;
+        },
 
-      if (seconds < 10) {
-        seconds = `0${seconds}`;
-      }
+        formattedTimeLeft() {
+            const timeLeft = this.timeLeft;
+            const minutes = Math.floor(timeLeft / 60);
+            let seconds = timeLeft % 60;
 
-      return `${minutes}:${seconds}`;
+            if (seconds < 10) {
+                seconds = `0${seconds}`;
+            }
+
+            if (minutes <= 0) {
+                return `${seconds}`;
+            }
+            return `${minutes}:${seconds}`;
+        },
+
+        timeLeft() {
+            return this.timeLimit - this.timePassed;
+        },
+
+        timeFraction() {
+            const rawTimeFraction = this.timeLeft / this.timeLimit;
+            return (
+                rawTimeFraction - (1 / this.timeLimit) * (1 - rawTimeFraction)
+            );
+        },
     },
 
-    timeLeft() {
-      return TIME_LIMIT - this.timePassed;
+    watch: {
+        timeLeft(newValue) {
+            if (newValue === 0) {
+                this.onTimesUp();
+            }
+        },
+    },
+    mounted() {
+        bus.$on("start", this.startTimer);
+        bus.$on("reset", this.resetTimer);
+        bus.$on("completed", this.onDone);
     },
 
-    timeFraction() {
-      const rawTimeFraction = this.timeLeft / TIME_LIMIT;
-      return rawTimeFraction - (1 / TIME_LIMIT) * (1 - rawTimeFraction);
+    methods: {
+        onDone() {
+            clearInterval(this.timerInterval);
+            this.timePassed = this.timeLimit;
+        },
+        onTimesUp() {
+            clearInterval(this.timerInterval);
+            this.timePassed = 0;
+            this.timerInterval = null;
+            bus.$emit("inter_done");
+        },
+        resetTimer() {
+            clearInterval(this.timerInterval);
+            this.timePassed = 0;
+            this.timerInterval = null;
+        },
+        startTimer() {
+            if (this.timerInterval == null) {
+                this.timerInterval = setInterval(
+                    () => (this.timePassed += 1),
+                    1000
+                );
+            } else {
+                clearInterval(this.timerInterval);
+                this.timerInterval = null;
+            }
+        },
     },
-
-    remainingPathColor() {
-      const { alert, warning, info } = COLOR_CODES;
-
-      if (this.timeLeft <= alert.threshold) {
-        return alert.color;
-      } else if (this.timeLeft <= warning.threshold) {
-        return warning.color;
-      } else {
-        return info.color;
-      }
-    }
-  },
-
-  watch: {
-    timeLeft(newValue) {
-      if (newValue === 0) {
-        this.onTimesUp();
-      }
-    }
-  },
-
-  mounted() {
-    this.startTimer();
-  },
-
-  methods: {
-    onTimesUp() {
-      clearInterval(this.timerInterval);
-    },
-
-    startTimer() {
-      this.timerInterval = setInterval(() => (this.timePassed += 1), 1000);
-    }
-  }
 };
 </script>
 
-<style scoped lang="scss">
+<style lang="scss" scoped>
 .base-timer {
-  position: relative;
-  width: 300px;
-  height: 300px;
-
-  &__svg {
-    transform: scaleX(-1);
-  }
-
-  &__circle {
-    fill: none;
-    stroke: none;
-  }
-
-  &__path-elapsed {
-    stroke-width: 7px;
-    stroke: grey;
-  }
-
-  &__path-remaining {
-    stroke-width: 7px;
-    stroke-linecap: round;
-    transform: rotate(90deg);
-    transform-origin: center;
-    transition: 1s linear all;
-    fill-rule: nonzero;
-    stroke: currentColor;
-
-    &.green {
-      color: rgb(65, 184, 131);
-    }
-
-    &.orange {
-      color: orange;
-    }
-
-    &.red {
-      color: red;
-    }
-  }
-
-  &__label {
-    position: absolute;
+    position: relative;
     width: 300px;
     height: 300px;
-    top: 0;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    font-size: 48px;
-  }
+
+    &__svg {
+        transform: scaleX(-1);
+    }
+
+    &__circle {
+        fill: none;
+        stroke: none;
+    }
+
+    &__path-elapsed {
+        stroke-width: 7px;
+        stroke: grey;
+    }
+
+    &__path-remaining {
+        stroke-width: 7px;
+        stroke-linecap: round;
+        transform: rotate(90deg);
+        transform-origin: center;
+        transition: 1s linear all;
+        fill-rule: nonzero;
+        stroke: currentColor;
+        color: #dc372f !important;
+    }
+
+    &__label {
+        position: absolute;
+        width: 300px;
+        height: 300px;
+        top: 0;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        font-size: 120px;
+    }
+    &__sub_label {
+        position: absolute;
+        width: 300px;
+        height: 200px;
+        top: 25%;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        font-size: 50px;
+    }
 }
 </style>
